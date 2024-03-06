d0a26bcc700dfb9e497fa29223c8865020e8551f
David <david.ehrenberg@rwth-aachen.de>
2024-03-06 20:35:38 +0100

    added publisher for block position

--- rebel_webots/rebel_webots/rebel_webots_driver.py
+++ rebel_webots/rebel_webots/rebel_webots_driver.py
@@ -14,6 +14,7 @@
 
 import rclpy
 from trajectory_msgs.msg import JointTrajectory
+from geometry_msgs.msg import Point
 
 class RebelWebotsDriver():
     def init(self, webots_node, properties):
@@ -26,9 +27,24 @@ class RebelWebotsDriver():
         self.__joint6 = self.__robot.getDevice('joint6')
         self.__left_finger_joint = self.__robot.getDevice('ROBOTIQ 2F-85 Gripper::left finger joint')
         self.__positions = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+        self.__block1 = self.__robot.getFromDef('block1_solid')
+        self.__block2 = self.__robot.getFromDef('block2_solid')
+
         rclpy.init(args=None)
         self.__node = rclpy.create_node('igus_webots_driver')
+        self.__pub1 = self.__node.create_publisher(Point, 'position/block1', 10)
+        self.__pub2 = self.__node.create_publisher(Point, 'position/block2', 10)
+        #self.__node.create_timer(1.0, self.publish_position(self.__block1_position))
         self.__node.create_subscription(JointTrajectory, 'joint_trajectory/command', self.__trajectory_callback, 1)
+
+    def publish_position(self,object, publisher):
+        #if position != [float('nan'),float('nan'),float('nan')]:
+        position = object.getPosition()
+        msg = Point()
+        msg.x = position[0]
+        msg.y = position[1]
+        msg.z = position[2]
+        publisher.publish(msg)
 
     def __trajectory_callback(self, trajectory):
         self.__positions = trajectory.points[-1].positions
@@ -42,6 +58,8 @@ class RebelWebotsDriver():
 
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
+        self.publish_position(self.__block1, self.__pub1)
+        self.publish_position(self.__block2, self.__pub2)
 
 def main(args=None):
     rclpy.init(args=args)
