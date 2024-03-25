02de9411a25211b92b5f684fe563cc6f487b9af5
David <david.ehrenberg@rwth-aachen.de>
2024-03-25 12:18:49 +0100

    velocity controller and object position publisher

--- rebel_webots/rebel_webots/rebel_webots_driver.py
+++ rebel_webots/rebel_webots/rebel_webots_driver.py
@@ -13,36 +13,32 @@
 #  limitations under the License.
 
 import rclpy
-from trajectory_msgs.msg import JointTrajectory
+from geometry_msgs.msg import Point
 
 class RebelWebotsDriver():
     def init(self, webots_node, properties):
         self.__robot = webots_node.robot
-        self.__joint1 = self.__robot.getDevice('joint1')
-        self.__joint2 = self.__robot.getDevice('joint2')
-        self.__joint3 = self.__robot.getDevice('joint3')
-        self.__joint4 = self.__robot.getDevice('joint4')
-        self.__joint5 = self.__robot.getDevice('joint5')
-        self.__joint6 = self.__robot.getDevice('joint6')
-        #self.__left_finger_joint = self.__robot.getDevice('ROBOTIQ 2F-85 Gripper::left finger joint')
-        #self.__positions = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-        self.__positions = [0.0,0.0,0.0,0.0,0.0,0.0]
+        self.__block1 = self.__robot.getFromDef('block1_solid')
+        self.__block2 = self.__robot.getFromDef('block2_solid')
+
         rclpy.init(args=None)
         self.__node = rclpy.create_node('igus_webots_driver')
-        self.__node.create_subscription(JointTrajectory, 'joint_trajectory/command', self.__trajectory_callback, 1)
+        self.__pub1 = self.__node.create_publisher(Point, 'position/block1', 10)
+        self.__pub2 = self.__node.create_publisher(Point, 'position/block2', 10)
 
-    def __trajectory_callback(self, trajectory):
-        self.__positions = trajectory.points[-1].positions
-        self.__joint1.setPosition(self.__positions[0])
-        self.__joint2.setPosition(self.__positions[1])
-        self.__joint3.setPosition(self.__positions[2])
-        self.__joint4.setPosition(self.__positions[3])
-        self.__joint5.setPosition(self.__positions[4])
-        self.__joint6.setPosition(self.__positions[5])
-        #self.__left_finger_joint.setPosition(self.__positions[6])
+    def publish_position(self,object, publisher):
+        #if position != [float('nan'),float('nan'),float('nan')]:
+        position = object.getPosition()
+        msg = Point()
+        msg.x = position[0]
+        msg.y = position[1]
+        msg.z = position[2]
+        publisher.publish(msg)
 
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
+        self.publish_position(self.__block1, self.__pub1)
+        self.publish_position(self.__block2, self.__pub2)
 
 def main(args=None):
     rclpy.init(args=args)
