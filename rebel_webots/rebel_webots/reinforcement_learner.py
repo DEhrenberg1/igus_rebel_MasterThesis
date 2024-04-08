c40d5211cabb491b043be0fca988edc40919164d
David <david.ehrenberg@rwth-aachen.de>
2024-04-08 15:58:00 +0200

    WIP: Towards RL, RL controls all 6 links now

--- rebel_webots/rebel_webots/reinforcement_learner.py
+++ rebel_webots/rebel_webots/reinforcement_learner.py
@@ -44,7 +44,7 @@ class ReinforcementLearnerEnvironment(gym.Env):
         self.__pos_gripper_set = False
         self.__distance_set = False
         ## OpenAI Stuff
-        self.action_space = spaces.Box(low = -1.0, high = 1.0, shape = (1,), dtype=np.float64) #Velocity controller of first joint
+        self.action_space = spaces.Box(low = -1.0, high = 1.0, shape = (6,), dtype=np.float64) #Velocity controller of all six joints
         self.observation_space = spaces.Dict(
             {
                 "rebel_arm_position": spaces.Box(self.__neg_inf, self.__pos_inf, shape= (6,), dtype=np.float64),
@@ -67,7 +67,7 @@ class ReinforcementLearnerEnvironment(gym.Env):
     ## Rewriting step and reset function of OpenAIGym for our Use Case:
     def step(self, action):
         print(action)
-        self.move_arm([action[0], 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.move_arm([action[0], action[1], action[2], action[3], action[4], action[5]])
         self.__current_steps += 1
         ##Condition for when the action is completed
         action_executed = False
@@ -83,7 +83,7 @@ class ReinforcementLearnerEnvironment(gym.Env):
         observation = {"rebel_arm_position": self.__arm_positions, "rebel_arm_velocity": self.__arm_velocities, "distance_to_block": self.__distance_gripper_b1}
         #observation = {"distance_to_block": self.__distance_gripper_b1}
 
-        reward = -self.__distance_gripper_b1
+        reward = 1 if self.reached_grasp_pose(0.02,0.02,0.01) else 0
         terminated = False
         truncated = True if (self.__current_steps >= self.__max_steps) else False
         info = {}
@@ -166,8 +166,25 @@ class ReinforcementLearnerEnvironment(gym.Env):
         point.positions = [pos]
         msg.points.append(point)
         self.__gripper_publisher.publish(msg)
+    
+    ##Functions used to compute the reward from the observation:
 
+    def compute_gripping_pos_center(self):
+        #computes grasping position of gripper above object
+        x = self.__block1_x
+        y = self.__block1_y
+        z = self.__block1_z + 0.14
 
+        return x,y,z
+    
+    def reached_grasp_pose(self, x_offset, y_offset, z_offset):
+        #returns true if a plausible grasp pose is reached (where plausible is defined by the offset)
+        x,y,z = self.compute_gripping_pos_center()
+        check_x = abs(x - self.__gripper_x) < x_offset
+        check_y = abs(y - self.__gripper_y) < y_offset
+        check_z = abs(z - self.__gripper_z) < z_offset
+
+        return check_x and check_y and check_z
 
 def main(args = None):
     rclpy.init(args=args)
@@ -183,7 +200,7 @@ def main(args = None):
     new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])  
     model.set_logger(new_logger)
     model.learn(total_timesteps = 10000, log_interval=1)
-    model.save("ddpg_igus_rebel_test2")
+    #model.save("ddpg_igus_rebel_test2")
 
 if __name__ == '__main__':
     main()
